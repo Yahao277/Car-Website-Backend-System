495c35d04eec94c0368ac6c5af3457e9fd256d0b
01100010 <jiahao.e21@gmail.com>
2021-05-23 22:11:00 +0200

    first commit

---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,42 @@
+package com.udacity.pricing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void requestTest() throws Exception {
+		mvc.perform(
+				get(new URI("/services/price?vehicleId=1"))
+						.accept(MediaType.APPLICATION_JSON_UTF8)
+		)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+	}
+
+}
